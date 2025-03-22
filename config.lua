381c27c5e9d894ff32b3a341a5e1b54ea6c1bc0b
nessie3122 <emre.ez287@gmail.com>
2025-03-22 21:34:01 +0300

    config.lua dosyası oluşturuldu

---
+++ config.lua
@@ -0,0 +1,14 @@
+Config = {}
+
+-- Discord yetkililerinin ID'leri
+Config.AuthorizedDiscordIDs = {
+    "DISCORD_ID_1",
+    "DISCORD_ID_2",
+    -- Buraya daha fazla Discord ID ekleyebilirsiniz
+}
+
+-- Discord webhook URL'si (log için)
+Config.WebhookURL = "WEBHOOK_URL_BURAYA"
+
+-- Komut adı
+Config.CommandName = "jobadmin"
